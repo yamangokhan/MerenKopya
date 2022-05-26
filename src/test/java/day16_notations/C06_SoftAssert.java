fe211067d9003fce6b1f39a5bb2ece9a0d204900
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-05-26 22:59:57 +0300

    05/26

--- src/test/java/day16_notations/C06_SoftAssert.java
+++ src/test/java/day16_notations/C06_SoftAssert.java
@@ -22,22 +22,34 @@ public class C06_SoftAssert extends TestBase {
         //1- amazon sayfasina gidin
         driver.get("https://www.amazon.com");
         //2- title'in Amazon icerdigini test edin
-        String expectedTitle= "amazon";
+        String expectedTitle= "Amazon";
         String actualTitle= driver.getTitle();
-        softAssert.assertTrue(actualTitle.contains(expectedTitle));
+        softAssert.assertTrue(actualTitle.contains(expectedTitle),"title amazon icermiyor");
         //3- aram kutusunun erisebilir oldugunu kontrol edin
         WebElement aramaKutusu= driver.findElement(By.id("twotabsearchtextbox"));
-        softAssert.assertTrue(aramaKutusu.isEnabled());
+        softAssert.assertTrue(aramaKutusu.isEnabled(),"arama kutusuna erisilemiyor");
         //4- arama kutusuna nutella yazip aratin
         aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
         //5- arama yapildigini test edin
         WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
-        softAssert.assertTrue(sonucYaziElementi.isDisplayed());
+        softAssert.assertTrue(sonucYaziElementi.isDisplayed(),"arama yapilamadi");
         //6- arama sonucunun Nutella icerdigini test edin
-        softAssert.assertTrue(sonucYaziElementi.getText().contains("Kutella"));
+        softAssert.assertTrue(sonucYaziElementi.getText().contains("Nutella"),"sonuc yazisi Kutella icermiyor");
 
         //sofassert'e bitis satirini soylemek icin assertAll() kullanilir
+        //`bu satir yazilmazsa assertion gorevi yapilamamis olur
         softAssert.assertAll();
+
+        /*
+        softassert'un hata bulsa bile calismaya devam etme ozelligi
+        assertAll()'a kadardir
+        Eger assertAll()'da failed rapor edilirse calisma durur
+        ve classin kalan kismi calistirilmaz
+        (Yani assertAll hardAsserdeki her bir assert gibidir, hatayi yakalarsa calisma durur)
+         */
+
+        System.out.println("assertion'lardan fail olan olursa burasi calismaz");
+        System.out.println("fail olan yoksa calisir");
 
     }
 }
