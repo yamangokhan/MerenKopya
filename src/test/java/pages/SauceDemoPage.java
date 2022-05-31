684452f8e73aa5c2751c0044727a3c999789b22d
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-05-31 19:19:55 +0300

    05/31

---
+++ src/test/java/pages/SauceDemoPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class SauceDemoPage {
+
+    public SauceDemoPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//input[@id=\"user-name\"]")
+    public WebElement username;
+
+    @FindBy(xpath = "//input[@id=\"password\"]")
+    public WebElement password;
+
+    @FindBy(xpath = "//input[@id=\"login-button\"]")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//select[@class='product_sort_container']")
+    public WebElement dropDown;
+
+    @FindBy(className = "inventory_item")
+    public List<WebElement> urunler;
+
+}
