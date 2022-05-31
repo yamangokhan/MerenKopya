331e61ccfa35da56c95acf80cd8d2aa0503ba14e
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-05-31 23:22:57 +0300

    05/31

---
+++ src/test/java/tests/practise/Q6_DropDown_FakerClass.java
@@ -0,0 +1,6 @@
+package tests.practise;
+
+public class Q6_DropDown_FakerClass {
+    //twitter a faker class kullanarak mail ile kayit olmaya calisiniz
+    //actions class kullanmayin twitteri deneyen instagram i denesin
+}
