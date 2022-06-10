fd078a23a03ff09fb700f4c3287fd34d0c2e0c4a
yamangokhan <yamangokhanyaman@gmail.com>
2022-06-10 14:55:51 +0300

    huseyin bey tavsiye :)

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -7,7 +7,7 @@ import utilities.Driver;
 
 public class AmazonPage {
 
-    public AmazonPage(){//parametresiz constructor
+    AmazonPage(){//parametresiz constructor
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
